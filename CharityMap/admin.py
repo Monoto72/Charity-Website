648ac2c50505fa6022ddd9f37043fe431aabab32
UP2017778 <sking233952@gmail.com>
2022-02-21 07:12:21 +0000

    added(Google-Map-Preview): Taken from https://www.gyford.com/phil/writing/2017/03/16/django-admin-map/ will rebuild, once geoDjango works

---
+++ CharityMap/admin.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib import admin
+from .models import Store
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'latitude', 'longitude',)
+    search_fields = ('name',)
+
+    fieldsets = (
+        (None, {
+            'fields': ( 'name', 'type', 'latitude', 'longitude',)
+        }),
+    )
+
+    class Media:
+        if hasattr(settings, 'GOOGLE_MAPS_API_KEY') and settings.GOOGLE_MAPS_API_KEY:
+            css = {
+                'all': ('css/admin/location_picker.css',),
+            }
+            js = (
+                'https://maps.googleapis.com/maps/api/js?key={}'.format(settings.GOOGLE_MAPS_API_KEY),
+                'js/location_picker.js',
+            )
