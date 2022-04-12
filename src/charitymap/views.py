05604f6e9d1597444c06e20f5d4ed5f7a31579b9
UP2017778 <sking233952@gmail.com>
2022-04-12 12:29:36 +0100

    fix(authentication-request): Removed needless refreshing, allowing for a user can see the errors, etc.

--- src/charitymap/views.py
+++ src/charitymap/views.py
@@ -73,13 +73,14 @@ def login_req(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
+                messages.info(request, f"You successfully logged in as {username}.")
                 return redirect("/")
             else:
                 messages.error(request,"Invalid username or password.")
         else:
             messages.error(request,"Invalid username or password.")
-    form = AuthenticationForm()
+    else:
+        form = AuthenticationForm()
     return render(request, "login.html", { "login_form": form, "page_url": url})
 
 
